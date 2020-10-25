c24da1c2fd8468317a398c70e764ea9af1655ced
Prskalo Ivan <iprskalo@fourier.cs.iit.edu>
2020-10-24 23:47:33 -0500

    Added working hash and bubblesort

--- labEC/bubblesort.c
+++ labEC/bubblesort.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 void bubblesort(int [], int);
 void swap(int*,int*);
 
 int main()
 {
-	printf("edit the bubblesort.c file to implement your bubble sort\n");
+	int BUFF = 50;
+	int list[10];
+	FILE *file;
+	file = fopen("pp2.txt","r");
+	char str[BUFF];
+	fgets(str, BUFF, file);
+	char * split = strtok(str," ");
+	int i=0;
+	while(split != NULL){
+		list[i]=atoi(split);
+		i++;
+		split = strtok(NULL," ");
+	}
+	int size = sizeof(list)/sizeof(list[0]);
+	printf("INPUT: ");
+	for(i=0; i<10; i++){
+		printf("%d ",list[i]);
+	}
+	printf("\n");
+	bubblesort(list, size);
+	printf("OUTPUT: ");
+	for(i=0; i<10; i++){
+		printf("%d ",list[i]);
+	}
+	printf("\n");
 	return 0;
 }
 
+void bubblesort(int list[], int i){
+	for(int j = 0; j<i-1; j++){
+		for(int k = 0; k<i-j-1; k++){
+			if(list[k+1]<list[k]){
+				swap(&list[k+1], &list[k]);
+			}
+		}
+	}
+}
+
+void swap(int *first, int *second){
+	int temp = *first;
+	*first = *second;
+	*second = temp;
+}
--- labEC/hash.c
+++ labEC/hash.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 int hash(char*);
 
-int main()
-{
-	printf("edit the hash.c file to implement your hashing functions\n");
+int main(int argc, char *argv[]){
+	if(argc > 2){
+		printf("This command uses only one argument. If your argument has spaces please use quotations");
+		return(-1);
+	} else if(argc == 1){
+		printf("This command uses one argument. You inputed no arguments");
+		return(-1);
+	}
+	printf("OUTPUT: %d\n", hash(argv[1]));
 	return 0;
 }
 
+int hash(char *arr){
+	int size = strlen(arr);
+	int i;
+	int total=0;
+	for(i=0;i<size;i++){
+		total+=arr[i];
+	}
+	return total;
+}
